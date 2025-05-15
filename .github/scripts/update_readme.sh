f895f3944448c15cb565ed07759773533a5367dc
tai-fan-new <yuepeng.lin-1@student.uts.edu.au>
2025-05-16 01:37:38 +1000

    Update update_readme.sh

--- .github/scripts/update_readme.sh
+++ .github/scripts/update_readme.sh
@@ -8,3 +8,8 @@ echo -e "\n### Update by $GITHUB_USER at $TIMESTAMP\n$FREQ_RESULT" >> README.md
 
 git config --global user.name "github-actions"
 git config --global user.email "github-actions@users.noreply.github.com"
+
+git add README.md
+git commit -m "Auto update README with vowel frequency" || echo "No changes to commit"
+git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}
+git push origin main
